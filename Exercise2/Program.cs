a5793466b9f6db9f3ac525a9484a2573c36213cc
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-10-31 16:26:49 +0700

    Membuat method input

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -7,5 +7,29 @@ namespace Exercise2
         int[] attar = new int[33];
         int n;
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Masukkan jumlah elemen array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (attar[n] <= 33))
+                    break;
+                else
+                    Console.WriteLine("\nArray maksimum 33 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(" Masukkan elemen array ");
+            Console.WriteLine("-----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                attar[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
